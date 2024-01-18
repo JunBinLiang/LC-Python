cb00e4fd9aa847c82ac206b9f161255f87c6b713
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-01-18 17:08:21 -0500

    Update 56.py

--- Interval/56.py
+++ Interval/56.py
@@ -2,9 +2,12 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
         stk = []
+        #situations:
+        # overlapping
+        # non-overlapping
         for interval in intervals:
             if len(stk) > 0 and interval[0] <= stk[len(stk) - 1][1]:
                 stk[len(stk) - 1][1] = max(stk[len(stk) - 1][1], interval[1])
-            else:
+            else: 
                 stk.append(interval)
         return stk
