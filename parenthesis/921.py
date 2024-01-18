f9014843d8556382350a815cd8246ccdf29b1353
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-01-18 17:56:31 -0500

    Create 921.py

---
+++ parenthesis/921.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        left = 0
+        answer = 0
+        for c in s:
+            if c == '(':
+                left += 1
+            else:
+                if left == 0:
+                    answer += 1
+                else:
+                    left -=1
+                    
+        return answer + left
+                
